b4e0724d429bae83e0be3ae3468cb2787a6d0273
liamelui <ilyadziamidchyk@gmail.com>
2025-07-11 19:53:54 +0200

    Added support for Build 42 to GL4ES renderer Capitalized the first letter of the app name Made installation instructions in the wiki more detailed and clear Styled the wiki page to match the app's theme Removed FMOD logging libraries from the bundled libs Modified FMOD loading to use explicit paths to libraries Added a settings option to choose the audio API (AAudio/OpenSL) Adjusted window insets to use bottom padding to avoid navbar overlap Added donation option to the main menu Improved logging, fixed issue when log files were erased after file manager was opened Added game files verification (simple check if files for Linux exist) Fixed app crashing after being put in background (GL4ES renderer) Temporarily disabled glTexEnvi in GL4ES renderer to stop visual glitches near cars (until root cause is found) Bumped version to 1.2.0 (3)

--- app/src/main/java/com/zomdroid/game/GameInstanceManager.java
+++ app/src/main/java/com/zomdroid/game/GameInstanceManager.java
@@ -8,35 +8,39 @@ import android.content.SharedPreferences;
 import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import com.zomdroid.C;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-public class GameInstancesManager {
-    private static GameInstancesManager singleton;
+public class GameInstanceManager {
+    private static GameInstanceManager singleton;
     private ArrayList<GameInstance> gameInstances = new ArrayList<>();
     private Gson gson = new Gson();
     private SharedPreferences sharedPreferences;
 
-
-    private GameInstancesManager(Context applicationContext) {
+    private GameInstanceManager(Context applicationContext) {
         this.sharedPreferences = applicationContext.getSharedPreferences(C.shprefs.NAME, MODE_PRIVATE);
-        loadInstancesFromDisk();
-    }
-    public static void init(Context applicationContext) {
-        singleton = new GameInstancesManager(applicationContext);
+        loadFromPreferences();
     }
 
-    public static GameInstancesManager getSingleton() {
+    public static void init(Context applicationContext) {
+        singleton = new GameInstanceManager(applicationContext);
+    }
+
+    public static GameInstanceManager getSingleton() {
         return singleton;
     }
 
     @NonNull
-    public static GameInstancesManager requireSingleton() {
+    public static GameInstanceManager requireSingleton() {
         if (singleton == null) {
-            throw new RuntimeException("GameInstancesManager is not initialized");
+            throw new RuntimeException("GameInstanceManager is not initialized");
         }
         return singleton;
     }
@@ -48,41 +52,47 @@ public class GameInstancesManager {
 
     public GameInstance getInstanceByName(String name) {
         for (GameInstance gameInstance : this.gameInstances) {
-            if (gameInstance.getName().equals(name))
-                return gameInstance;
+            if (gameInstance.getName().equals(name)) return gameInstance;
         }
         return null;
     }
 
     public void registerInstance(@NonNull GameInstance gameInstance) {
         this.gameInstances.add(gameInstance);
-        saveInstancesToDisk();
+        saveToPreferences();
     }
 
     public void unregisterInstance(@NonNull GameInstance gameInstance) {
         this.gameInstances.remove(gameInstance);
-        saveInstancesToDisk();
+        saveToPreferences();
     }
 
-    public void setInstanceInstalled(@NonNull GameInstance gameInstance) {
-        gameInstance.setInstalled(true);
-        saveInstancesToDisk();
+    public void markInstallationFinished(@NonNull GameInstance gameInstance) {
+        gameInstance.markInstallationFinished();
+        saveToPreferences();
     }
 
-    private void loadInstancesFromDisk() {
+    private void loadFromPreferences() {
         String json = this.sharedPreferences.getString(C.shprefs.keys.GAME_INSTANCES, null);
         if (json != null) {
-            Type type = new TypeToken<ArrayList<GameInstance>>(){}.getType();
-            ArrayList<GameInstance> savedInstances = gson.fromJson(json, type);
-            if (savedInstances != null) {
-                gameInstances.addAll(savedInstances);
+            JsonArray rawArray = JsonParser.parseString(json).getAsJsonArray();
+            for (JsonElement element : rawArray) {
+                JsonObject obj = element.getAsJsonObject();
+
+                GameInstance instance = gson.fromJson(obj, GameInstance.class);
+
+                if (obj.has("isInstalled")) { // compat v1.1.0
+                    boolean isInstalled = obj.get("isInstalled").getAsBoolean();
+                    if (isInstalled) instance.markInstallationFinished();
+                }
+
+                gameInstances.add(instance);
             }
         }
     }
 
-    private void saveInstancesToDisk() {
+    private void saveToPreferences() {
         String json = this.gson.toJson(this.gameInstances);
         this.sharedPreferences.edit().putString(C.shprefs.keys.GAME_INSTANCES, json).apply();
     }
-
 }
